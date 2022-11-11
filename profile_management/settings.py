7902e2e951869f23b79b5694dcc1d29226ab1541
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2022-11-11 19:32:56 +0100

    added django restframework, jwt authentication basics

--- profile_management/settings.py
+++ profile_management/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# REST FRAMEWORK Settings
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ]
+}
 
 # Application definition
 
@@ -37,6 +46,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "profiles",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
