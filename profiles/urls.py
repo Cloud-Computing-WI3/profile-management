2e322be443cae59bd21c4e106d7fc1c35bfcc187
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2022-11-22 09:32:20 +0100

    Rebase that shit

--- profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from profiles.views import ProfileView
-
-namespace = "profiles"
-
-urlpatterns = [
-    path("", ProfileView.as_view({"get": "list", "post": "create", "put": "update", "delete": "destroy"})),
-    path("<int:pk>", ProfileView.as_view({"get": "retrieve", "delete": "destroy"})),
-]
