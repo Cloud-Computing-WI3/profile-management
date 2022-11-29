85368bf10c231a36db4b4b79b248370f605837cb
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2022-11-29 17:02:07 +0100

    added new models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from accounts.models import Account
+from accounts.models import Account, Category, Keyword
 
 admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Keyword)
