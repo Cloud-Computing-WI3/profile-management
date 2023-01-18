cae78cef0c693da61d9283ea5eb571e8ce32bbac
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2023-01-18 16:08:18 +0100

    merge

--- core/settings.py
+++ core/settings.py
@@ -21,15 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "4LjeiHkz$&@un5B68c2Kn4%hm4W*N4nMBoD8NtxS%mJjxX9!F5oj!U*AT3^B76@*f&hFSdPN*Y%pcDLCMg5ii"
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECRET_KEY = "django-insecure-^dfd+2vfj9df%)gofr7h72b@-0f*h8=p31@6tyjaf8edk7g+0*"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "profile-management-2qda3nwega-uc.a.run.app"]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://cohesive-slate-368310.uc.r.appspot.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 # Application definition
 AUTH_USER_MODEL = "accounts.Account"
 
@@ -96,16 +94,26 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "profile_management",
-        "USER": "admin",
-        "PASSWORD": "W*2mLDsXQy7TYc",
-        "HOST": "profile-management.c9epzfgto5cy.us-east-1.rds.amazonaws.com",
-        "PORT": "3306",
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'dev_database.sqlite',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": "profile_management",
+            "USER": "YOUR-DB-USER",
+            "PASSWORD": "YOUR-DB-PASSWORD",
+            "HOST": "YOUR-AWS-RDS-HOST-NAME",
+            "PORT": "3306",
+        }
+    }
+
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
@@ -168,21 +176,21 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # File Uploads
-AWS_ACCESS_KEY_ID = "AKIAT4ACZN3WATI5FKQ7"
-AWS_SECRET_ACCESS_KEY = "oLk7XuFUp/8W1O06C4dFaAIIz6jIp8NmAR+a135j"
-AWS_STORAGE_BUCKET_NAME = "profile-management-m-ngucu959yxpxdmrffqra1buedz47ruse1b-s3alias"
+AWS_ACCESS_KEY_ID = "YOUR-AWS-ACCESS-KEY-ID-HERE"
+AWS_SECRET_ACCESS_KEY = "YOUR-AWS-SECRET-ACCESS-KEY-HERE"
+AWS_STORAGE_BUCKET_NAME = "YOUR-S3-BUCKET-NAME-HERE"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_REGION_NAME = "us-east-1"
-AWS_S3_FILE_OVERWRITE = True
+AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
